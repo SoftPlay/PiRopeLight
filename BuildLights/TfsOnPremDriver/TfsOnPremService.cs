e8b37da8d8dfef38964d80a190063a56677353b3
Pete Stensønes <peter@stensones.com>
2016-10-24 19:17:52 +0100

    start of on prem TFS driver implementation

---
+++ BuildLights/TfsOnPremDriver/TfsOnPremService.cs
@@ -0,0 +1,17 @@
+﻿namespace TfsOnPremDriver
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Infrastucture;
+
+	public class TfsOnPremService : ITfsService
+	{
+		public Task<IQueryable<string>> GetCompletedBuilds(string project)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
